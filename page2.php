533aa4a7cdb5b9029be7f2c1c6271aa5bf7be3a9
Muhammad Shehroz Sajjad <14bscsmsajjad@seecs.edu.pk>
2018-01-04 07:02:23 +0500

    page 2 complete

--- page2.php
+++ page2.php
@@ -1,72 +1,85 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Research User Behaviour</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-    <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
-    <!-- Custom Font Icons CSS-->
-    <link rel="stylesheet" href="https://file.myfontastic.com/BQ5rqoUxsfQGHC35jWa5Ub/icons.css">
-    <!-- Google fonts - Open Sans-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800">
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="css/custom.css">
-    <!-- Favicon-->
-    <link rel="shortcut icon" href="favicon.png">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-  </head>
-  <body>
-    <section>
-      <div class="container">
-        <div class="row block">
-          <div class="col-lg-12">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Research User Behaviour</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="all,follow">
+  <!-- Bootstrap CSS-->
+  <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome CSS-->
+  <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+  <!-- Custom Font Icons CSS-->
+  <link rel="stylesheet" href="https://file.myfontastic.com/BQ5rqoUxsfQGHC35jWa5Ub/icons.css">
+  <!-- Google fonts - Open Sans-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800">
+  <!-- theme stylesheet-->
+  <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+  <!-- Custom stylesheet - for your changes-->
+  <link rel="stylesheet" href="css/custom.css">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="favicon.png">
+  <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+</head>
+<body>
+  <section>
+    <div class="container">
+      <div class="row block">
+        <div class="col-lg-12">
 
-            <p class="lead">So let’s start in Albany, New York. You live in a lovely old house but all of a sudden the faucet starts leaking like crazy and spouting water. Your run downstairs and turn off the water.
-            </p>
-            <p class="lead">
-              Next stop, Google. <br/>
-            <strong>And you key in</strong>
-            </p>
-            <div class="form-holder">
-            <section  id="newsletter" class="newsletter">
-              <div class="container">
-                <form id="newsletterForm" action="#">
-                  <div class="form-group">
-                    <input type="text" name="email" id="email" value="Emergency, plumber, Albany"  disabled>
-                    <button type="submit" class="btn btn-primary btn-gradient submit">Search</button>
+          <p class="lead">So let’s start in Albany, New York. You live in a lovely old house but all of a sudden the faucet starts leaking like crazy and spouting water. Your run downstairs and turn off the water.
+          </p>
+          <p class="lead">
+            Next stop, Google. <br/>
+
+          </p>
+          <h3 style="margin-bottom:30px;">And you key in</h3>
+          <div class="form-holder">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-6 col-lg-9 mx-auto">
+                  <div class="searchBox">
+                    <div class=" google-logo mx-auto text-center">
+                      <img src="http://freewp.world/google-logo/google.png" alt="logo" class="img-fluid">
+
+                    </div>
+                    <div class=" google-form text-center">
+                      <form action="page3.php" method="get">
+                        <div class="form-group">
+                          <input type="text" disabled class="form-control google-search" value="Emergency, plumber, Albany" name="search">
+                          <div class="btn-group">
+                            <button type="submit" class="btn btn-default">Google Search</button>
+                            <button type="submit" class="btn btn-default" disabled>I'm Feeling Lucky</button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
                   </div>
-                </form>
+                </div>
               </div>
-
-            </section>
-
-        </div>
-            <p class="lead">
-              Now hit enter and go select your plumber, quick before the kitchen floods!
-            </p>
-
             </div>
+
+          </div>
+          <h3 style="margin-top:30px;">
+            Now hit search and go select your plumber, quick before the kitchen floods!
+          </h3>
+
         </div>
-
       </div>
-    </section>
+
+    </div>
+  </section>
 
 
-    <!-- Javascript files-->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="js/popper.min.js"> </script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
-    <script src="js/front.js"></script>
-  </body>
+  <!-- Javascript files-->
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/popper.min.js"> </script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+
+</body>
 </html>
