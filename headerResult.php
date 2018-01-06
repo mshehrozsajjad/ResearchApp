6312c05d78a4c4a6bf74e058fb19280aa8d67e08
Muhammad Shehroz Sajjad <14bscsmsajjad@seecs.edu.pk>
2018-01-06 06:06:30 +0500

    Search Result Page Layout

---
+++ headerResult.php
@@ -0,0 +1,25 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Research User Behaviour</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="all,follow">
+  <!-- Bootstrap CSS-->
+  <!-- <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css"> -->
+  <!-- Font Awesome CSS-->
+  <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+  <!-- Custom Font Icons CSS-->
+  <link rel="stylesheet" href="https://file.myfontastic.com/BQ5rqoUxsfQGHC35jWa5Ub/icons.css">
+  <!-- Google fonts - Open Sans-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800">
+  <!-- theme stylesheet-->
+  <link rel="stylesheet" href="css/searchresult.css">
+  <!-- Custom stylesheet - for your changes-->
+  <!-- <link rel="stylesheet" href="css/custom.css"> -->
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="favicon.png">
+  <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+</head>
--- page7.php
+++ page7.php
@@ -1,80 +1,210 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Research User Behaviour</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="robots" content="all,follow">
-  <!-- Bootstrap CSS-->
-  <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-  <!-- Font Awesome CSS-->
-  <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
-  <!-- Custom Font Icons CSS-->
-  <link rel="stylesheet" href="https://file.myfontastic.com/BQ5rqoUxsfQGHC35jWa5Ub/icons.css">
-  <!-- Google fonts - Open Sans-->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800">
-  <!-- theme stylesheet-->
-  <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
-  <!-- Custom stylesheet - for your changes-->
-  <link rel="stylesheet" href="css/custom.css">
-  <!-- Favicon-->
-  <link rel="shortcut icon" href="favicon.png">
-  <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-</head>
+<?php
+require 'headerResult.php';
+?>
+
 <body>
   <section>
     <div class="container">
-      <div class="row block">
-        <div class="col-lg-12">
-          <h1>Next Challenge</h1>
-          <p class="lead">You got tired of that old house so moved to Erie, Pennsylvania. You’ve been shovelling snow in Erie, and you throw your back out so you key in</p>
-
-          <div class="form-holder">
-            <div class="container ">
-              <div class="row">
-                <div class="col-md-6 col-lg-9 mx-auto">
-                  <div class="searchBox">
-                    <div class=" google-logo mx-auto text-center">
-                      <img src="http://freewp.world/google-logo/google.png" alt="logo" class="img-fluid">
-
-                    </div>
-                    <div class=" google-form text-center">
-                      <form action="page5.php" method="post">
-                        <div class="form-group">
-                          <input type="text" disabled class="form-control google-search" value="best chiropractor, Erie" name="search">
-                          <div class="btn-group">
-                            <button type="submit" class="btn btn-default">Google Search</button>
-                            <button type="submit" class="btn btn-default" disabled>I'm Feeling Lucky</button>
-                          </div>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
+      <div class="top-bar">
+        <div class="left-side">
+          <img id="main-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/272px-Google_2015_logo.svg.png">
+          <input type="text" id="search-bar" value="Emergency, plumber, Albany" disabled>
+          <img id="mic" src="https://1.bp.blogspot.com/-lINg3E20_4Y/VebwdAy_MaI/AAAAAAAAFJU/8Jg2H-3-_cc/s1600/Microphone.png">
+          <div class="mag-background">
+            <span id="mag"></span>
           </div>
-          <h3 style="margin-top:30px;">
-            Hit Go!
-          </h3>
-
+        </div>
+        <div class="right-side">
+          <ul>
+            <li id="nine-square"><a  ></a></li>
+            <li id="circle-bell"><a  ></a></li>
+            <li id="profile-pic"></li>
+          </ul>
         </div>
       </div>
+      <div class="scnd-bar">
+        <div class="scnd-left">
+          <ul class="nav-list">
+            <li><a id="active"  >All</a></li>
+            <li><a  >News</a></li>
+            <li><a  >Videos</a></li>
+            <li><a  >Maps</a></li>
+            <li><a  >Images</a></li>
+            <li><a  >More</a><span class="arrow-down"></span></li>
+            <li><a  >Search tools</a></li>
+          </ul>
+        </div>
+        <div class="scnd-right">
+          <div class="user">
+            <div id="user-span"></div>
+          </div>
+          <div class="globe">
+            <div id="globe-span"></div>
+          </div>
+          <div class="settings">
+            <div id="settings-span"></div>
+          </div>
+        </div>
+      </div>
+      <div class="results">
+        <p id="num-results">About 10,760,000,000 results (0.49 seconds)</p>
 
-    </div>
-  </section>
+        <!-- Start Ads -->
+        <div class="search-results">
+          <h2><a  href="page4.php?p=3&t=1" >24Hr Emergency Plumbers - Same Day Repair & Service‎</a></h2>
+          <p><span class="_mB">Ad</span>
+            <a   class="link">www.plumbersnearyou.com/</a><p>
+              <p class="summary">
+                Enter Zip Code for Local Emergency Plumbers. Call now for Experts.
+                Trusted professionals · Call for appointment now · Fast & Reliable · Exhaustive directory
+                Services: Water heaters, Sump pumps, Septic systems, Pipe work
+              </p>
+            </div>
+            <div class="search-results">
+              <h2><a  >Emergency Plumbers - Serving Greater Albany Area‎</a></h2>
+              <p><span class="_mB">Ad</span>
+                <a   class="link">www.rolandjdown.com/Plumbing‎</a><p>
+                  <p class="summary">
+                    Prompt, Professional Plumbing and Water Heater Service 24/7. $25 Off Coupon.
+                    Expert Plumbing Repairs · Certified Plumbers · 24/7 Emergency Service · Satisfaction Guaranteed
+                    Services: Drain Cleaning, Water Heater Service, Leak Detection/Repair, Sewer Services
+                  </p>
+                </div>
+                <div class="search-results">
+                  <h2><a  >Now Hiring Plumbers in Albany - $2000 Signing Bonus + Benefits‎‎</a></h2>
+                  <p><span class="_mB">Ad</span>
+                    <a   class="link">www.crisbro.com/employment/</a><p>
+                      <p class="summary">
+                        Join Crisafulli Bros. Proud to be the Best in the Region for Over 75 Years!
+
+                      </p>
+                    </div>
+
+                    <!-- End Ads -->
+
+                    <div class="card">
+                      <img src="https://www.elegantthemes.com/blog/wp-content/uploads/2016/09/Divi-Google-Maps.png" alt="Avatar" style="width:100%">
+                      <div class="container">
+                        <div class="search-results border">
+                          <h2 style="color:black;"><a >Crisafulli Bros. Plumbing & Heating Contractors</a></h2>
+                          <p>
+                            <a class="summary">520 Livingston Ave · +1 518-449-1782</a><p>
+                              <p class="summary">
+                                Open 24 hrs
+                              </p>
+                        </div>
+                        <div class="search-results border">
+                          <h2><a >Roto-Rooter Plumbing & Drain Services</a></h2>
+                          <p>
+                            <a class="summary">60 Dott Ave · +1 518-438-3831</a><p>
+                              <p class="summary">
+                              Open 24 hrs
+                              </p>
+                            </div>
+                            <div class="search-results border">
+                              <h2><a >Empire State Plumbing, Inc.</a></h2>
+                              <p>
+                                <a class="summary">
+260 Osborne Rd · +1 518-482-4205</a><p>
+                                  <p class="summary">
+                                  Open 24 hrs
+                                  </p>
+                            </div>
+                      </div>
+                    </div>
+                    <!-- Organic Links -->
+                    <div class="search-results">
+                      <h2><a >Albany Plumbing Service & Repair - (24/7 Emergency Plumbing ...</a></h2>
+                      <p>
+                        <a class="link">www.crisbro.com/plumbing-service-repair.php</a><p>
+                          <p class="summary">
+                            Plumbing Service and Repair for Albany NY. Whether you need a faucet replaced, your pipes unthawed or a sump pump installed to keep your basement dry, our Albany plumbing repair and service contractors can help.
+                          </p>
+                        </div>
+                        <div class="search-results">
+                          <h2><a >Albany, NY Plumber | Roto-Rooter Plumbing & Drain Services</a></h2>
+                          <p>
+                            <a class="link">https://www.rotorooter.com</a><p>
+                              <p class="summary">
+                                Trusted and recommended since 1935, Roto-Rooter is the premier provider of plumbing and drain cleaning services in Albany, NY. Homeowners and businesses depend on Roto-Rooter 24 hours a day, 7 days a week. Our specialty is emergency services. Your call will be answered by a trained customer service  ...
+                              </p>
+                            </div>
+                            <div class="search-results">
+                              <h2><a >Albany, OR Plumber | Roto-Rooter Plumbing & Drain Services
+                              </a></h2>
+                              <p>
+                                <a class="link">https://www.rotorooter.com</a><p>
+                                  <p class="summary">
+                                    Albany Plumber, Providing Emergency Plumbing and Drain Service. Roto-Rooter plumbers in Albany provide plumbing maintenance and repairs as well as clogged drain cleaning. Roto-Rooter's Albany plumbers offer residential and commercial plumbing services that customers depend on for all of their local plumbing ...
+                                  </p>
+                                </div>
+                                <div class="search-results">
+                                  <h2><a >Albany Plumbing Service & Repair - (24/7 Emergency Plumbing ...</a></h2>
+                                  <p>
+                                    <a class="link">www.crisbro.com/plumbing-service-repair.php</a><p>
+                                      <p class="summary">
+                                        Plumbing Service and Repair for Albany NY. Whether you need a faucet replaced, your pipes unthawed or a sump pump installed to keep your basement dry, our Albany plumbing repair and service contractors can help.
+                                      </p>
+                                    </div>
+                                    <div class="search-results">
+                                      <h2><a >25 Best Plumbers - Albany NY - HomeAdvisor</a></h2>
+                                      <p>
+                                        <a class="link">https://www.homeadvisor.com</a><p>
+                                          <p class="summary">
+                                            New Lead. Project: Install, Repair or Replace Plumbing or Fixtures - For Business . Location: Albany, NY. Date: 01/04/2018. Is this an emergency?: No. Request Stage: Ready to Hire. Desired Completion Date: Timing is flexible. What kind of location is this?: Business. Comment: Need the flusher replaced in our toilet or have ...
+                                          </p>
+                                        </div>
+                                        <div class="search-results">
+                                          <h2><a >Plumbers & Emergency Plumbing Services in Albany, NY - YP.com</a></h2>
+                                          <p>
+                                            <a class="link">https://www.yellowpages.com/albany-ny/plumbers</a><p>
+                                              <p class="summary">
+                                                282 results - Local plumbers in Albany, NY. Compare expert Plumbers, read reviews, and find contact information - THE REAL YELLOW PAGES®
+                                              </p>
+                                            </div>
+
+                                            <div class="related-searches">
+                                              <h3>Searches related to ipsum lorem</h3>
+                                              <table>
+                                                <tr>
+                                                  <td><a  >ipsum lorem <strong>generator</strong></a></td>
+                                                  <td><a  >ipsum lorem <strong>hipster</strong></a></td>
+                                                </tr>
+                                                <tr>
+                                                  <td><a  >ipsum lorem <strong>bacon</strong></a></td>
+                                                  <td><a  >ipsum lorem <strong>word</strong></a></td>
+                                                </tr>
+                                                <tr>
+                                                  <td><a  >ipsum lorem <strong>meaning</strong></a></td>
+                                                  <td><a  >ipsum lorem <strong>samuel l jackson</strong></a></td>
+                                                </tr>
+                                                <tr>
+                                                  <td><a  >ipsum lorem <strong>generator funny</strong></a></td>
+                                                  <td><a  >ipsum lorem <strong>dolor english translation</strong></a></td>
+                                                </tr>
+                                              </table>
+                                            </div>
+                                          </div>
+                                          <div class="footer">
+                                            <div id="location"></div><p>Tysons, VA - From your Internet address - <span id="location-search">Use precise location - Learn more</span></p>
+                                            <ul>
+                                              <li><a >Help</a></li>
+                                              <li><a >Send feedback</a></li>
+                                              <li><a >Privacy</a></li>
+                                              <li><a >Terms</a></li>
+                                            </ul>
+                                          </div>
+                                        </div>
+                                      </section>
 
 
-  <!-- Javascript files-->
-  <script src="js/jquery-3.2.1.min.js"></script>
-  <script src="js/popper.min.js"> </script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-  <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+                                      <!-- Javascript files-->
+                                      <script src="js/jquery-3.2.1.min.js"></script>
+                                      <script src="js/popper.min.js"> </script>
+                                      <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+                                      <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
 
-</body>
-</html>
+                                    </body>
+                                    </html>
